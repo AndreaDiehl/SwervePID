29bf1e80182b4b66020a150b39a543efd9fba585
AndreaDiehl <93095800+AndreaDiehl@users.noreply.github.com>
2022-02-11 16:03:11 -0600

    micah was here

---
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class ExampleCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  public
+  static IntakeSubsystem m_IntakeSubsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param m_autoCommand The subsystem used by this command.
+   */
+  public ExampleCommand(IntakeSubsystem subsystem) {
+    m_IntakeSubsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_IntakeSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
